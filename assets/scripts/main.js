0a0c6c254bbd3e818104b1583eb271af5bb5f8c6
DanielChenOfficial2 <53456446+DanielChenOfficial2@users.noreply.github.com>
2021-02-03 00:05:24 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,19 @@
 // main.js
 
 // TODO
+let volnumElement = document.getElementById('.volume-number');
+volnumElement.addEventListener('input', (event) => {
+  let result = volnumElement.valueAsNumber();
+  let newalevel = horn-sound.getElementById();
+  newalevel.volume = result * 0.01;
+});
+let volsliderElement = document.getElementById('.volume-slider');
+volsliderElement.addEventListener('input, (event) => {
+  let result = volsliderElement.valueAsNumber();
+  let newalevel = horn-sound.getElementById();
+  newalevel.volume = result * 0.01;
+});
+let useralevel = volume-number.valueAsNumber();
+let newalevel = horn-sound.getElementById();
+alevel.volume = useralevel * 0.01;
+
